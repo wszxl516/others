81121b50633001d5333da6d18f0f63613ec1a5ce
wszxl516 <wszxl516@163.com>
2017-01-22 21:59:33 +0800

    Delete 00.py

--- 00.py
+++
@@ -1,9 +0,0 @@
-import subprocess as subpro
-def run(cmd):
-    pro = subpro.Popen(cmd,bufsize=1024, stdout=subpro.PIPE, stderr=subpro.PIPE, shell=False, env={"PATH": "/system/bin:/bin"})
-    while pro.stdout.readable():
-        print(pro.stdout.readline().decode())
-    while pro.stderr.readable():
-        print("ERROR:")
-        print(pro.stderr.read())
-run(['ping', 'baidu.com'])
