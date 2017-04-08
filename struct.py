7d00e2040719ba5fe63dd7bb8ddbd38f94abd921
wszxl516 <wszxl516@163.com>
2017-04-08 18:05:03 +0800

    Update struct.py

--- struct.py
+++ struct.py
@@ -9,3 +9,23 @@ if __name__ == '__main__':
     res = so.test()
     print (res.contents.name)
     print (res.contents.age)
+#C代码
+'''
+#include <stdio.h>  
+#include <string.h>  
+#include <stdlib.h>  
+  
+typedef struct StructPointerTest  
+{  
+    char name[20];  
+    int age;  
+}StructPointerTest, *StructPointer;  
+  
+StructPointer test()    // 返回结构体指针  
+{   
+    StructPointer p = (StructPointer)malloc(sizeof(StructPointerTest));   
+    strcpy(p->name, "Joe");  
+    p->age = 20;  
+      
+    return p;   
+}  '''
