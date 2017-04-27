2f5bcc0f6cf5231da3f6e2eb207b9ec28e62af95
wszxl516 <wszxl516@163.com>
2017-04-27 21:18:56 +0800

    linux显示cpu与内存使用率

--- getinfo.c
+++ getinfo.c
@@ -42,11 +42,12 @@ void getdata(int *all)
 char temp[10][10]={0},tmp='\0';
 int i = 0,x = 0;
 FILE *fp=NULL;
+int size=0;
 int flag=0;
 fp = fopen("/proc/stat", "r");
 while(tmp!=10)
 {
-    fread(&tmp,1,1,fp);
+    size = fread(&tmp,1,1,fp);
     if(tmp>=48&&tmp<=57)
     {
         temp[i][x] = tmp;
@@ -69,12 +70,13 @@ float meminfo()
 char total[10],free[10], tmp, t;
 FILE *fp=NULL;
 int all=0,used=0,i=0;
+int size=0;
 fp = fopen("/proc/meminfo", "r");
 if(fp == NULL)
 printf("file not exist!");
 while(tmp!=10)
 {
-    fread(&tmp,1,1,fp);
+    size = fread(&tmp,1,1,fp);
     if(tmp>=48&&tmp<=57)
     {
        total[i]=tmp;
@@ -88,7 +90,7 @@ fseek(fp,i,0);
 i = 0;
 while(t!=10)
 {
-fread(&t,1,1,fp);
+size = fread(&t,1,1,fp);
 if((int)t>=48&&(int)t<=57)
 {
     free[i]=t;
