ec8a1f55c53b38963eefd39851c04fedad6f0047
wszxl516 <wszxl516@163.com>
2017-04-27 20:59:42 +0800

    linux显示cpu与内存使用率

--- 000.c
+++ 000.c
@@ -1,32 +1,67 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-void getinfo(int *num);
+void getdata(int *all);
 float meminfo();
+float getinfo(); 
 int main(void)
 {
-float mem=0.0;
-int num[8];
-getinfo(num);
+float mem=0.0,cpu=0.0;
+while(1)
+{
+cpu = getinfo();
 mem = meminfo();
-printf("%2.2f%% \n",mem);
+printf("\33\[?25l\33\[s \33[1mCPU:\33\[32m%2.2f%%\33\[37m MEM:\33\[32m%2.2f%%\33\[u\33\[?25h\33\[0m",cpu,mem);
+fflush(stdout);
+}
 return 0;
 }
-
-void getinfo(int *num)
+float getinfo()
 {
-char temp[128], tmp='\0', *tt;
+int all1[10],all[10],i;
+int total=0,user=0;
+getdata(all);
+sleep(1);
+getdata(all1);
+for(i=0;i<8;i++)
+{
+    //printf("%d -- %d\n",all[i],all1[i]);
+    total += all1[i] - all[i];
+    if(i<3)
+    {
+        user += all1[i] - all[i];
+    }
+
+}
+//printf("%d -- %d\n",user,total);
+return (float)(user*100)/(float)(total);
+}
+void getdata(int *all)
+{
+char temp[10][10]={0},tmp='\0';
+int i = 0,x = 0;
 FILE *fp=NULL;
-int i=0;
+int flag=0;
 fp = fopen("/proc/stat", "r");
-printf("read file!");
 while(tmp!=10)
 {
     fread(&tmp,1,1,fp);
-    temp[i]=tmp;
-    printf("temp[i] %s\n",temp[i]);
-    i++;
+    if(tmp>=48&&tmp<=57)
+    {
+        temp[i][x] = tmp;
+        flag = 1;
+        x++;
+    }
+    if(tmp == 32 && flag)
+    {
+        x = 0;
+        flag = 0;
+        all[i] = atol((const char*)&temp[i]);
+        i++;
+    }
 }
+
 fclose(fp);
 }
 float meminfo()
@@ -40,7 +75,7 @@ printf("file not exist!");
 while(tmp!=10)
 {
     fread(&tmp,1,1,fp);
-    if((int)tmp>=48&&(int)tmp<=57)
+    if(tmp>=48&&tmp<=57)
     {
        total[i]=tmp;
        i++;
@@ -60,7 +95,7 @@ if((int)t>=48&&(int)t<=57)
     i++;
 }
 }
-used = atoi(free);
-
-return 100*(all-used)/all;
+used = all - atoi(free);
+//printf("%d -- %d\n",used,all);
+return 100*used/all;
 }
