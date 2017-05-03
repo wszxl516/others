ab2e04387d9c427df8a617efbd4fcd9dcfade826
wszxl516 <wszxl516@163.com>
2017-05-03 22:07:03 +0800

    q

--- mappingport.py
+++ mappingport.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
-#encoding:utf-8
+# encoding:utf-8
 import socket
 import threading
 import sys
 SIZE = 4096
 
+
 class Test:
-    def __init__(self, host, hostport, mapport ):
+    def __init__(self, host, hostport, mapport):
         sock_svr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock_svr.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock_svr.bind(('localhost', mapport))
@@ -14,18 +15,22 @@ class Test:
         self.server, addr = sock_svr.accept()
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((host, hostport))
+
     def clients(self):
         while True:
             data = self.client.recv(SIZE)
             print('recv:', len(data))
             self.server.send(data)
         self.client.close()
+
     def servers(self):
         while True:
             data = self.server.recv(SIZE)
             print('send:', len(data))
             self.client.send(data)
         self.erver.close()
+
+
 if __name__ == '__main__':
     if len(sys.argv) >= 4:
         t = Test(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
@@ -34,5 +39,5 @@ if __name__ == '__main__':
         t2.start()
         t1.start()
     else:
-        print('Usage:\n\t{0} [remote addr] [remote port] [local port]'.format(sys.argv[0]))
-        
+        print('Usage:\n\t{0} [remote addr] [remote port] [local port]'
+              .format(sys.argv[0]))
