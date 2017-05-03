9e8daf40517e70f9ef1af19fd85a441cfe51476a
wszxl516 <wszxl516@163.com>
2017-05-03 09:34:14 +0800

    Update p2a.py

--- p2a.py
+++ p2a.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3.5
+#encoding=utf-8
 from PIL import Image
 import sys
 charlist = ['o', '#', '$', '%', '&', '?', '*', 'o', '/', '{', '[', '(', '|',\
@@ -6,25 +7,26 @@ charlist = ['o', '#', '$', '%', '&', '?', '*', 'o', '/', '{', '[', '(', '|',\
 count = len(charlist)
 
 def toText(image_file):
-    image_file=image_file.convert("L")#转灰度
-    asd =''#储存字符串
-    for h in range(0,  image_file.size[1]):#hasattr
-        for w in range(0, image_file.size[0]):#warning
+    image_file=image_file.convert("L")
+    asd =''
+    for h in range(0,  image_file.size[1]):
+        for w in range(0, image_file.size[0]):
             gray = image_file.getpixel((w,h))
             asd += charlist[int(gray/(255/(count-1)))]
             asd += '\r\n'
     return asd
 
 def toText2(image_file):
-    asd =''#储存字符串
-    for h in range(0,  image_file.size[1]):#hasattr
+    asd =''
+    for h in range(0,  image_file.size[1]):
         for w in range(0, image_file.size[0]):
-            if sys.argv[1].endswith('.png'):
-                r,g,b,a =image_file.getpixel((w,h))
-                gray =int(r* 0.2 + g*0.3 + b*0.1 + 0.2*a)
+            color = image_file.getpixel((w,h))
+            if color.__len__() == 4:
+                r, g, b, a = color
+                gray = int(r* 0.2+g* 0.5+b* 0.1+0.2*a)
             else:
-                r,g,b =image_file.getpixel((w,h))
-                gray =int(r* 0.299+g* 0.587+b* 0.114)
+                r, g, b = color
+                gray = int(r* 0.299+g* 0.587+b* 0.114)
             asd += charlist[int(gray/(255/(count-1)))]
         asd += '\r\n'
     return asd
@@ -37,11 +39,11 @@ def main():
     if sys.argv.__len__() == 3:
         size = sys.argv[2].split('x')
         size = [int(i) for i in size]
-    image_file = Image.open(sys.argv[1]) # 打开图片
+    image_file = Image.open(sys.argv[1])
     if not size:
         size = image_file.size
     image_file=image_file.resize((int(size[0]),
-                             int(size[1])))#调整图片大小
+                             int(size[1])))
     print('name:{}'.format(sys.argv[1]),'size:',image_file.size[0],'x',image_file.size[1])
     print(toText2(image_file))
 if __name__ == '__main__':
