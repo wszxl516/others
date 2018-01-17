f8c99c9f073ecb91e47ff3dfabfd3a27224b2a8c
Zhang <wszxl516@163.com>
2018-01-17 18:22:27 +0800

    Update run.c

--- run.c
+++ run.c
@@ -1,24 +1,42 @@
 #include <Python.h>
 
-int main()
-
+int main(int argc, char **argv)
 {
 
+    int ret;
     Py_Initialize();                             
-    PyObject * pModule = NULL;    //声明变量  
-    PyObject * pFunc = NULL;      //声明变量 
+    PyObject * pModule = NULL; 
+    PyObject * pFunc = NULL;  
+    PyObject * pArgs = NULL;
+    PyObject * pResult = NULL;
+    if(argc > 1)
+    {
+        pArgs = PyTuple_New(argc);
+        for(int i=1;i<argc;i++)
+        {
+             PyTuple_SetItem(pArgs,i-1,Py_BuildValue("s", argv[i]));
+             printf("%s\n", argv[i]);
+         }   
+    }
     PyRun_SimpleString("import sys");
     PyRun_SimpleString("sys.path.append('./')");     
-    pModule =PyImport_ImportModule("ip");
-    if (!pModule)
+    pModule =PyImport_ImportModule("test_run");
+    if (pModule == NULL)
     {
-     printf("can't find module!");
-     return -1;
+        printf("Can't find module test_run!");
+        return -1;
     }
-    pFunc= PyObject_GetAttrString(pModule, "main");
-    PyEval_CallObject(pFunc, NULL);  
+    pFunc= PyObject_GetAttrString(pModule, "test");
+    if (pFunc == NULL)
+    {
+        printf("Can't find function test!");
+        return -1;
+    }
+    printf("pArgs is %p\n",pArgs);
+    pResult = PyEval_CallObject(pFunc, pArgs);  
+    PyArg_Parse(pResult, "i", &ret);
+    printf("result = %d\n", ret);
     Py_Finalize();                          
-
     return 0;
 
 }
