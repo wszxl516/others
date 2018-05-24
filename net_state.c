2a0a0dbe9863fb28b9f21e477fa215e2897473ea
Zhang <wszxl516@163.com>
2018-05-24 21:31:27 +0800

    Update net_state.c

--- net_state.c
+++ net_state.c
@@ -8,6 +8,8 @@
 #include <net/if.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+
 
 typedef unsigned short u16;
 typedef unsigned int u32;
@@ -15,6 +17,69 @@ typedef unsigned char u8;
 
 #include <linux/ethtool.h>
 #include <linux/sockios.h>
+
+
+
+int get_info(int skfd, char *ifname)
+{
+        struct ifreq ifr;
+        char mac[21] = {0};
+	char ip[16] = {0};
+	char broadcast[16] = {0};
+	char netmask[16] = {0};
+
+
+        strncpy(ifr.ifr_name, ifname, IFNAMSIZ);
+        //mac address
+        if (!ioctl(skfd, SIOCGIFHWADDR, &ifr))
+        {
+            memset(mac, 0, sizeof(mac));
+            snprintf(mac, sizeof(mac), "%02x:%02x:%02x:%02x:%02x:%02x",
+                (unsigned char)ifr.ifr_hwaddr.sa_data[0],
+                (unsigned char)ifr.ifr_hwaddr.sa_data[1],
+                (unsigned char)ifr.ifr_hwaddr.sa_data[2],
+
+                (unsigned char)ifr.ifr_hwaddr.sa_data[3],
+                (unsigned char)ifr.ifr_hwaddr.sa_data[4],
+                (unsigned char)ifr.ifr_hwaddr.sa_data[5]);
+            printf("mac: %s\n", mac);
+
+        }
+        else
+        {
+            printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
+            return -1;
+        }
+	
+	 //ip address
+        if (!ioctl(skfd, SIOCGIFADDR, &ifr))
+        {
+
+            strcpy(ip, inet_ntoa(((struct sockaddr_in*)&(ifr.ifr_addr))->sin_addr));
+            printf("ip: %s\n", ip);
+
+
+        }
+        else
+        {
+            printf("ioctl: %s [%s:%d]\n", strerror(errno), __FILE__, __LINE__);
+            return -1;
+        }
+	//broadcast
+	if(!ioctl(skfd, SIOCGIFBRDADDR, &ifr))
+	{
+		strcpy(broadcast, inet_ntoa(((struct sockaddr_in *)&ifr.ifr_broadaddr)->sin_addr));
+		printf("broadcast:%s\n", broadcast);
+
+       }
+       //netmask
+       if(ioctl(skfd, SIOCGIFNETMASK, &ifr) != -1)
+	{
+		strcpy(netmask, inet_ntoa(((struct sockaddr_in *)&ifr.ifr_broadaddr)->sin_addr));
+		printf("netmask:%s\n", netmask);
+	}
+}
+
 
 int detect_mii(int skfd, char *ifname)
 {
@@ -52,7 +117,6 @@ int detect_ethtool(int skfd, char *ifname)
 {
         struct ifreq ifr;
         struct ethtool_value edata;
-
         memset(&ifr, 0, sizeof(ifr));
         edata.cmd = ETHTOOL_GLINK;
 
@@ -62,12 +126,11 @@ int detect_ethtool(int skfd, char *ifname)
         if (ioctl(skfd, SIOCETHTOOL, &ifr) == -1) 
         {
                 printf("ETHTOOL_GLINK failed: %s\n", strerror(errno));
-                return 2;
+                return  detect_mii(skfd, ifname);
         }
 
         return (edata.data ? 0 : 1);
 }
-
 int main(int argc, char **argv)
 {
         int skfd = -1;
@@ -87,14 +150,8 @@ int main(int argc, char **argv)
                 exit(-1);
         }
 
-        retval = detect_ethtool(skfd, ifname);
-
-        if (retval == 2)
-                retval = detect_mii(skfd, ifname);
-
-
-        close(skfd);
-
+	retval = detect_ethtool(skfd, ifname);
+	printf("status:");
         if (retval == 2)
                 printf("Could not determine status\n"); 
 
@@ -102,7 +159,10 @@ int main(int argc, char **argv)
                 printf("Link down\n");
 
         if (retval == 0)
-                printf("Link up\n");
-
+	{
+		printf("Link up\n");
+		get_info(skfd, ifname);
+	}
+	close(skfd);
         return retval;
 }
